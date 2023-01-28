9dde51232bb9311d342687d828134f2204b3ddb4
tramwai <MIHAIL.HARITON@GMAIL.COM>
2023-01-28 14:09:00 -0600

    "added test5 locator fix"

--- src/test/java/pages/UnitedFlightResultsPage.java
+++ src/test/java/pages/UnitedFlightResultsPage.java
@@ -8,11 +8,14 @@ public class UnitedFlightResultsPage extends UnitedBasePage {
         super();
     }
 
+    // @FindBy(xpath = "//*[@id=\"flightResults-content\"]/div[2]/div/div/div/div[1]/div/span")  -> full xpath works
     ///@FindBy(css = "div[class$='sticky--1-Jdl'] span b")
-    //*[@id="flightResults-content"]/div[2]/div/div/div/div[1]/div/span
     //(//div[@class$='sticky--1-Jdl'] span b)[1]
     // (//div[@id='flightResults-content']//b)[1]/..
     // //div[#'flightResults-content']/div/div/span[@innertext=' February 28']
-    @FindBy(xpath = "//*[@id=\"flightResults-content\"]/div[2]/div/div/div/div[1]/div/span")
+    //  @FindBy(xpath = "(//div[@id='flightResults-content']//b)[1]/../..")
+
+    // css = ".app-components-Shopping-ColumnSortResultHeader-styles__detailHeading--2d8nC > span"
+    @FindBy(xpath = "(//div[@id='flightResults-content']//b)[2]/..")
     public WebElement departureDateConfirmation;
 }
